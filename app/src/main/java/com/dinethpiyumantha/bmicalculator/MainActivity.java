43d44784a3bf287d2038f00353e79f0a24687bff
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-03-03 09:50:58 +0530

    Bmi method implemented

--- app/src/main/java/com/dinethpiyumantha/bmicalculator/MainActivity.java
+++ app/src/main/java/com/dinethpiyumantha/bmicalculator/MainActivity.java
@@ -3,10 +3,19 @@ package com.dinethpiyumantha.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText h;
+    private EditText w;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        h = (EditText) findViewById(R.id.txtinp1);
+        w = (EditText) findViewById(R.id.txtinp2);
+        result = (TextView) findViewById(R.id.txtout);
+    }
+
+    public void calculateBmi(View view) {
+        int hinp = Integer.parseInt(h.getText().toString());
+        double winp = Double.parseDouble(w.getText().toString());
+        double r = winp/Math.pow((hinp/100),2);
+        result.setText(String.valueOf(r));
     }
 }
